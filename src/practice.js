7b1c58ee389d0edb46da41d74ca686b94dd26711
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-01 14:00:47 -0500

    completed whopipe function; starting drill environment

--- src/practice.js
+++ src/practice.js
@@ -45,3 +45,37 @@ function getProductsWithImages() {
 }
 
 // getProductsWithImages()
+
+
+/*
+SQL Syntax for following function:
+
+SELECT video_name, region, count(date_viewed) AS views
+FROM whopipe_video_views
+  WHERE date_viewed > (now() - '30 days'::INTERVAL)
+GROUP BY video_name, region
+ORDER BY region ASC, views DESC;
+
+*/
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      //PREPARED STATEMENT for Knex; prevents SQL injection
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC'}
+    ])
+    .then(result => {
+      console.log(result)
+    })
+}
+
+mostPopularVideosForDays(30);
