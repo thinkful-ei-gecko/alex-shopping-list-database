0bf2d2dd9c0b675e31b1a452e5c205f4a98bb13b
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-01 13:22:14 -0500

    amazong functions

--- src/practice.js
+++ src/practice.js
@@ -7,14 +7,41 @@ const knexInstance = knex({
 });
 console.log('knex and driver installed correctly');
 
-const qry = knexInstance
+function searchByProduceName(searchTerm) {
+  knexInstance
   .select('product_id', 'name', 'price', 'category')
   .from('amazong_products')
-  .where({ name: 'Point of view gun' })
-  .first()
-  .toQuery()
-  // .then(result => {
-  //   console.log(result);
-  // });
+  .where('name', 'ILIKE', `%${searchTerm}%`)
+  .then(result => {
+    console.log(result)
+  });
+}
 
-  console.log(qry);
+// searchByProduceName('holo');
+
+function paginateProducts(page) {
+  const productsPerPage = 10
+  const offset = productsPerPage * (page - 1)
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+// paginateProducts(2)
+
+function getProductsWithImages() {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category', 'image')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then(result => {
+      console.log(result)
+    })
+}
+
+// getProductsWithImages()
