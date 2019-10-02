0386b68ced407c5810fe1df305e963adecf24e70
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-02 02:32:15 -0500

    context testing for getting articles, articles-service.spec.js injections, hooks installed

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -3,6 +3,27 @@ const knex = require('knex');
 
 describe('Articles service object', function() {
   let db;
+  let testArticles = [
+    {
+      id: 1,
+      date_published: new Date('2029-01-22T16:28:32.615Z'),
+      title: 'First test post!',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
+    },
+    {
+      id: 2,
+      date_published: new Date('2100-05-22T16:28:32.615Z'),
+      title: 'Second test post!',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
+    },
+    {
+      id: 3,
+      date_published: new Date('1919-12-22T16:28:32.615Z'),
+      title: 'Third test post!',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
+    },
+  ];
+
   before(() => {
     db = knex({
       client: 'pg',
@@ -10,10 +31,37 @@ describe('Articles service object', function() {
     });
   });
 
+  afterEach(()=>  db('blogful_articles').truncate());
+
+  before(() => db('blogful_articles').truncate());
+
+  after(() => db.destroy());
+
   describe('getAllArticles()', () => {
     // eslint-disable-next-line quotes
-    it('resolves all articles from \'blogful_articles\'', () => {
-      //test that ArticlesService.getAllArticles gets data from table
+    context(`Given 'blogful_articles' has data`, () => {
+      before( () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles);
+      });
+      // eslint-disable-next-line quotes
+      it('getAllArticles() resolves all articles from \'blogful_articles\' table', () => {
+        //test that ArticlesService.getAllArticles gets data from table
+        return ArticlesService.getAllArticles(db)
+          .then(actual => {
+            expect(actual).to.eql(testArticles);
+          });
+      });
+    });
+
+    context(`Given 'blogful_articles' has no data`, () => {
+      it(`getAllArticles() resolves an empty array`, () => {
+        return ArticlesService.getAllArticles(db)
+          .then(actual => {
+            expect(actual).to.eql([]);
+          });
+      });
     });
   });
 });
